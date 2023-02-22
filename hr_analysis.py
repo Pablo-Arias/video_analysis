4bc7ea9dac91f17a40e8634a58cd9b5355c3eb93
Pablo Arias <>
2023-02-22 10:37:01 +0000

    exceptions

--- hr_analysis.py
+++ hr_analysis.py
@@ -31,7 +31,6 @@ def analyse_folder(sources, target_folder, wsize = 6, roi_approach = 'patches', 
 	"""
 
 	for file in glob.glob(sources):
-
 		wsize = 8 # seconds of video processed (with overlapping) for each estimate 
 		file_tag = get_file_without_path(file)
 		target_file = target_folder + file_tag + ".pickle"
@@ -39,38 +38,42 @@ def analyse_folder(sources, target_folder, wsize = 6, roi_approach = 'patches', 
 		if exists(target_file):
 			print(target_file + ' exists, skipping it')
 			continue
+		try:
+			# run
+			pipe = Pipeline()          # object to execute the pipeline
+			res = pipe.run_on_video(file,
+				                                        winsize=wsize, 
+				                                        roi_method='convexhull',
+				                                        roi_approach=roi_approach,
+				                                        method=method,
+				                                        estimate=bpm_est,
+				                                        patch_size=0, 
+				                                        RGB_LOW_HIGH_TH=(5,230),
+				                                        Skin_LOW_HIGH_TH=(5,230),
+				                                        pre_filt=True,
+				                                        post_filt=True,
+				                                        cuda=True, 
+				                                        verb=True)
 
-		# run
-		pipe = Pipeline()          # object to execute the pipeline
-		res = pipe.run_on_video(file,
-			                                        winsize=wsize, 
-			                                        roi_method='convexhull',
-			                                        roi_approach=roi_approach,
-			                                        method=method,
-			                                        estimate=bpm_est,
-			                                        patch_size=0, 
-			                                        RGB_LOW_HIGH_TH=(5,230),
-			                                        Skin_LOW_HIGH_TH=(5,230),
-			                                        pre_filt=True,
-			                                        post_filt=True,
-			                                        cuda=True, 
-			                                        verb=True)
+			# ERRORS
+			#RMSE, MAE, MAX, PCC, CCC, SNR = getErrors(bvps, fps, bpmES, bpmGT, timesES, timesGT)
+			#printErrors(RMSE, MAE, MAX, PCC, CCC, SNR)
+			#displayErrors(bpmES, bpmGT, timesES, timesGT)
 
-		# ERRORS
-		#RMSE, MAE, MAX, PCC, CCC, SNR = getErrors(bvps, fps, bpmES, bpmGT, timesES, timesGT)
-		#printErrors(RMSE, MAE, MAX, PCC, CCC, SNR)
-		#displayErrors(bpmES, bpmGT, timesES, timesGT)
+			## file
+			#print(bvps)
+			#print(timeES)
+			#print(bpmES)
+			#res = [bvps, timeES, bpmES]
 
-		## file
-		#print(bvps)
-		#print(timeES)
-		#print(bpmES)
-		#res = [bvps, timeES, bpmES]
-		with open(target_file, 'wb') as handle:
-			pickle.dump(res, handle, protocol=pickle.HIGHEST_PROTOCOL)
-#except:
-#print("An error occured analsing : " + file)
-#pass
+			with open(target_file, 'wb') as handle:
+				pickle.dump(res, handle, protocol=pickle.HIGHEST_PROTOCOL)
+		except
+			print("An error occured analsing : " + file)
+		
+		except KeyboardInterrupt:
+			print("interrupted")
+			sys.exit(0)
 
 
 if __name__ == "__main__":
